9bc7004d18d0215f7bcbc44242dd3b3652178c25
JRS Moura <jrsteiner78@gmail.com>
2021-10-01 21:10:53 -0300

    project squeletor

---
+++ apps/app2.py
@@ -0,0 +1,33 @@
+import dash
+import dash.html as html
+import dash.dcc as dcc
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+app.layout = html.Div([
+    html.H4('Data About EUA Agriculture (2011)'),
+    generate_table(df)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
